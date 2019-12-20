8518987d99ca7de7c48b85be91421f56a047ba3d
Jose Riguera <jriguera@gmail.com>
2019-12-20 19:45:33 +0100

    Define docker-compose actions

--- docker/compose.sh
+++ docker/compose.sh
@@ -1,10 +1,40 @@
 #!/bin/bash
 set -eo pipefail
 
-# if command starts with an option, prepend docker-compose
-if [[ "${1}" != 'docker-compose' ]]
-then
-	set -- docker-compose "$@"
-fi
+COMPOSE_FILE=${COMPOSE_FILE:-docker-compose.yml}
+COMPOSE_PROJECT_DIRECTORY=${COMPOSE_PROJECT_DIRECTORY:-$(dirname $COMPOSE_FILE)}
+COMPOSE_PROJECT_NAME=${COMPOSE_PROJECT_NAME:-system}
 
-exec "$@"
+case "${1}" in
+    start)
+        docker-compose rm -v --force
+        docker-compose pull --quiet
+        docker-compose up -d --no-color --remove-orphans
+        ;;
+    reload)
+        docker-compose pull --quiet
+        docker-compose build --pull
+        docker-compose up -d --no-color --remove-orphans
+        ;;
+    stop)
+        docker-compose down
+        ;;
+    status)
+        docker-compose ps
+        ;;
+    top)
+        docker-compose top
+        ;;
+    run)
+        shift
+        docker-compose "$@"
+        ;;
+    help|*)
+        echo "Usage:  $0 [ start | reload | stop | status | run <list-docker-compose-options> ]"
+        echo "  docker-compose wrapper to manage 'system' stack in <stackdir> folder"
+        echo "  Default <stackdir> folder: ${COMPOSE_PROJECT_DIRECTORY}"
+        echo "  Settings in <stackdir> (${COMPOSE_PROJECT_DIRECTORY}): ${COMPOSE_FILE}"
+        echo
+        ;;
+esac
+
